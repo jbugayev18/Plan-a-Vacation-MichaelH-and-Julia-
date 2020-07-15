ab414628c0256289dec6436fbc786d6609c8a05c
Eclipse <lpskirito7@outlook.com>
2020-07-15 16:20:50 -0400

    yayayayayaya

--- national_parks.js
+++ national_parks.js
@@ -1,26 +1,63 @@
-'use strict';
+"use strict";
 
-function getParks(searchTerm, limit=10){
-  fetch(
-    `https://developer.nps.gov/api/v1/parks?q=${searchTerm}&limit=${limit}&api_key=yEQDlJRh9YfjdZG9J5hGLQO1Qsb1LJXwSX2jQxKU`
-  )
-    .then((res)=> res.json)
-    .then((users) => users(searchTerm))
+const api = 'h6FnVWOybjmPd1awcytwbqWIlNsxfzv8FM7YrC8v';
+const parkSearch = 'https://developer.nps.gov/api/v1/parks';
 
+function getParks(state, limit=10){
+  const params = {
+    stateCode:state,
+    limit,
+    apiKey:api
+  };
+
+  const parkStr = userFormatParks(params);
+
+  const url = parkSearch + '?' + parkStr;
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => display(responseJson))
+    .catch(err => {
+      console.log(err.message);
+    });
 }
 
-function submit(e){
-  $('form').on(submit, function(e){
-    let searchTerm = $('#park').val(); 
-    getParks(searchTerm, 10);
+function userFormatParks(params){
+  const items = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+  return items.join("&");
+}
+
+function display(responseJson){
+  $('.parks').empty();
+  console.log(responseJson);
+
+  for(let i = 0; i < responseJson.data.length; i++){
+    $('.parks').append(`'
+   <li>
+   <h4>${responseJson.data[i].fullName}</h4>
+   <p>${responseJson.data[i].description}</p>
+   <a href="${responseJson.data[i].url}">For more info pleas click Here</a>
+   <address> 
+   ${responseJson.data[i].addresses[1].line1}<br>
+   ${responseJson.data[i].addresses[1].city}<br>
+   ${responseJson.data[i].addresses[1].stateCode}<br>
+   ${responseJson.data[i].addresses[1].postalCode}<br>
+   </address>
+   </li>`);
+  }
+}
+
+function imWatchingYou(){
+  $('form').submit(e => {
+    e.preventDefault();
+    const state = $('#search').val();
+    const limit = $('number').val();
+    getParks(state, limit);
   });
-  e.preventDefault();
-  
-
-
 }
-
-function render(users){
-  let park= users.map(('ul').append('<li>users.state</li><li>users.contacts</li>'));
-
-}
+$(imWatchingYou);
